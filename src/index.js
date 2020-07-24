81287dedbcc15dc455dabca5fe0bb4e7cbb73365
ulquiomaru <baris.gorgulu@gmail.com>
2020-07-25 01:56:13 +0300

    merge

--- src/index.js
+++ src/index.js
@@ -1,16 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom";
-<<<<<<< HEAD
 import App from "./components/App";
-=======
-import Game from "./components/Game";
->>>>>>> 2b326b5a6f788c67b0b6f156192e8f940985bcad
 import "./style/index.css";
 
 const rootElement = document.getElementById("root");
 
-<<<<<<< HEAD
 ReactDOM.render(<App />, rootElement);
-=======
-ReactDOM.render(<Game size={boardSize} />, document.getElementById("root"));
->>>>>>> 2b326b5a6f788c67b0b6f156192e8f940985bcad
